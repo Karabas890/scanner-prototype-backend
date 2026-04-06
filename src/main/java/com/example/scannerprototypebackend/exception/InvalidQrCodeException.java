d37bd21377777d1a9450564439142bb8f8315067
Karabas890 <ivan.mitichev@mail.ru>
2026-04-06 21:58:42 +0300

    add 400 Bad Request

--- src/main/java/com/example/scannerprototypebackend/exception/InvalidQrCodeException.java
+++ src/main/java/com/example/scannerprototypebackend/exception/InvalidQrCodeException.java
@@ -1,5 +1,9 @@
 package com.example.scannerprototypebackend.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidQrCodeException extends RuntimeException {
     public InvalidQrCodeException(String message) {
         super(message);
